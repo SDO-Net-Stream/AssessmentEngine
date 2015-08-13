f8be3d5b047c6a6d1dc062b55aa95f789e335fbf
serhii-kapura <sergiy.kapura@gmail.com>
2015-08-13 15:18:23 +0300

    Revert "Revert "Implemented basis of Admin and User controllers, viewmodels and views;""
    This reverts commit be850d571c1ed855fe85973664592dada44619eb.

---
+++ Olts/Olts.WebUi/Controllers/AdminController.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Linq;
+using System.Web.Mvc;
+using System.Web.Security;
+using Olts.WebUi.Models.Admin;
+using WebMatrix.WebData;
+
+namespace Olts.WebUi.Controllers
+{
+    [Authorize(Roles = "Administrator")]
+    public class AdminController : Controller
+    {
+        #region GET
+
+        [HttpGet]
+        public ActionResult Users()
+        {
+            var viewModel = new AdminViewModel();
+            return View(viewModel);
+        }
+        
+        [HttpGet]
+        public ActionResult Users(Int32 id)
+        {
+            return View();
+        }
+
+        #endregion
+
+        #region POST
+
+        [HttpPost]
+        public ActionResult Users(AdminViewModel viewModel)
+        {
+            return View(viewModel);
+        }
+
+        [HttpPost]
+        public ActionResult Create(UserViewModel user)
+        {
+            return new EmptyResult();   
+        }
+        
+        [HttpPost]
+        public ActionResult Update(UserViewModel user)
+        {
+            return new EmptyResult();
+        }
+
+        [HttpPost]
+        public ActionResult Delete(String name)
+        {
+            var roles = Roles.GetRolesForUser(name);
+            if (roles.Any())
+            {
+                Roles.RemoveUserFromRoles(name, roles);
+            }
+            var membershipProvider = (SimpleMembershipProvider) Membership.Provider;
+            var isAccountDeleted = membershipProvider.DeleteAccount(name);
+            var isUserDeleted = membershipProvider.DeleteAccount(name);
+            // TODO: Show appropriate message in case if user or account wasn't deleted successfully
+
+            return View("Users");
+        }
+
+        #endregion
+    }
+}
