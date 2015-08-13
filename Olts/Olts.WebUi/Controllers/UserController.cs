f1e71e3242f99a808cfd8f3cee0dba37a7a404d8
serhii-kapura <sergiy.kapura@gmail.com>
2015-08-13 19:30:53 +0300

    Add user controller actions;

--- Olts/Olts.WebUi/Controllers/UserController.cs
+++ Olts/Olts.WebUi/Controllers/UserController.cs
@@ -1,12 +1,35 @@
-﻿using System.Web.Mvc;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+using Olts.DataAccess;
+using Olts.Domain;
 
 namespace Olts.WebUi.Controllers
 {
     public class UserController : Controller
     {
+        #region GET
+
         public ActionResult Surveys()
         {
-            return View();
+            IEnumerable<Survey> viewModel;
+            using (var context = new OltsContext())
+            {
+                viewModel = context.Surveys.ToList();
+            }
+            return View(viewModel);
         }
+
+        public ActionResult Survey(Int32 id)
+        {
+            return View(new Survey());
+        }
+
+        #endregion
+
+        #region POST
+
+        #endregion
     }
 }
